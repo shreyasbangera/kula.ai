81d307e328903929eb8c370318be301c57e00825
shreyasbangera <shreyasybangera1@gmail.com>
2023-02-07 13:01:00 +0530

    updated css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Home from "./Home";
 
@@ -6,6 +7,7 @@ function App() {
   return <div className="App">
     <Navbar />
     <Home />
+    <Footer />
   </div>;
 }
 export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div style={{color:"gray", textAlign:"center", padding:"1rem"}}>©️Kula.ai</div>
+  )
+}
+
+export default Footer
